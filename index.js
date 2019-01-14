19e79660c9f934b41548e38c4015ec7ddd6528e3
damlois <lois0168@gmail.com>
2019-01-14 15:45:09 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const redis = require('redis');
+const multer = require('multer');
 
 const app = express();
 const port = process.env.PORT || 8700;
@@ -14,12 +15,29 @@ const client = redis.createClient();
         console.log('Something went wrong ' + err);
     });
 
+const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, './uploads')
+        },
+        filename: function (req, file, cb) {
+          cb(null, file.fieldname + '.txt')
+        }
+      })
+       
+const upload = multer({ storage: storage })
+
+// base endpoint
+app.get('/', function(req, res) {
+    res.json({ message: 'WELCOME' });   
+});
+
 //route to upload file to redis
-app.post('/upload', function(req,res){
- 
+app.post('/upload', upload.single('myFile'),function(req,res){
+    console.log(req.file);
     //read file from path and save it to redis database
-    fs.readFile('test.txt', function (err, data) {
-        if (err) res.send(err);
+    const file_path = './uploads/myFile.txt'
+    fs.readFile(file_path, function (err, data) {
+        if (err) res.send({error : err});
 
         dataFromFile = data.toString();
 
@@ -30,16 +48,20 @@ app.post('/upload', function(req,res){
             client.set(key,each);
         });  
 
-        res.send("File uploaded successfully");
+        res.send({error: false,
+                stausCode : 200,
+                message : 'File uploaded successfully'});
     });
 });
 
 //route to get file from redis
-app.get('/get/:id', (req,res) => {
-    let id = req.params.id;
-    client.get(id.toString(), (err,data) => {
-        if(err) res.send(err);
-        res.send(data);
+app.get('/msisdn', (req,res) => {
+    let id = req.query.id;
+    client.get(id.toString(), (err,value) => {
+        if(err) res.send({error : err});
+        res.send({error : false,
+                statusCode: 200,
+                data: value});
     });
 })
 
